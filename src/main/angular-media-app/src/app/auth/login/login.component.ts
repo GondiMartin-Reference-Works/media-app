8ac6949521fcb5e906025e094b956e9175df6dc6
martin.gondocs@gmail.com <GondiMartin>
2024-03-09 20:26:39 +0100

    MA-14 - Add Login Page

--- src/main/angular-media-app/src/app/auth/login/login.component.ts
+++ src/main/angular-media-app/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RegisterUser } from '../../models/register-user';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: RegisterUser = new RegisterUser();
 
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ){
+
+  }
+
+  ngOnInit(): void {}
+
+  
 }
