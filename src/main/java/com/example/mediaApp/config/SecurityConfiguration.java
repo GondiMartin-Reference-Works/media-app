afa0fe17a0f3859f908becf1d5ddbb7ccc6015dd
rvili20 <100083135+rvili20@users.noreply.github.com>
2024-03-15 12:59:47 +0100

    MA-61 Fix erroneous Spring security implementation

--- src/main/java/com/example/mediaApp/config/SecurityConfiguration.java
+++ src/main/java/com/example/mediaApp/config/SecurityConfiguration.java
@@ -3,7 +3,7 @@ package com.example.mediaApp.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+    private final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
