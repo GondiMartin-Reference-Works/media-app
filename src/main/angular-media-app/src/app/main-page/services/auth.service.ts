286cb6a6813b9e6c2066591bcda3ff23c45be708
martin.gondocs@gmail.com <martin.gondocs@gmail.com>
2024-03-24 13:26:05 +0100

    MA-16 - Fix code

--- src/main/angular-media-app/src/app/main-page/services/auth.service.ts
+++ src/main/angular-media-app/src/app/main-page/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  isLoggedIn  = true;
+  isLoggedIn  = false;
 
   checkLoging(): void{
     this.isLoggedIn = localStorage.getItem("current-user-token") != null;
